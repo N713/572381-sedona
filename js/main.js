a5dc9fb755aef9e42485d0de580b45830fb4d8e1
N713 <ely137137@gmail.com>
2018-04-03 01:57:04 +0300

    Предпроверка

--- js/main.js
+++ js/main.js
@@ -1,13 +1,19 @@
-'use strict'
 
 let openModalButton = document.getElementById('modalOpen'),
-  modalReserv = document.querySelector('.modal-find');
+  modalReserv = document.querySelector('.modal-find'),
+  formButtons = document.querySelectorAll('.button-form');
 
 
 openModalButton.addEventListener('click', function(){
   // modalReserv.classList.add('modal-active');
   modalReserv.classList.toggle('modal-active');
 });
+
+for(let i = 0; i < formButtons.length; i++){
+    formButtons[i].addEventListener('click', function(e){
+      e.preventDefault();
+    });
+}
 
 function initMap() {
   var uluru = {lat: 34.8697395, lng: -111.7609896};
